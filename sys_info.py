281d6b0a26e3eba193ed71642dfdf9dcf4f481f3
Darkest Surface <74876947+DrkTheDon@users.noreply.github.com>
2022-02-28 21:13:59 +0100

    Update Python Script

---
+++ sys_info.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#####################################################
+## DarkPentestToolInstallor (DrkPTI)               ##
+## A useful tool to download pentest tools  quick  ##
+## https://voidsecurity.ml                         ##
+## Coded by: drk                                   ##
+## NOTE: This will display info about your system  ##
+#####################################################
+
+# Imports
+import time
+import os
+import sys
